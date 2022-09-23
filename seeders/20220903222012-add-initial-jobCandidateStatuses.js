44a7870c0e8eca456d95ff0b74463730ce02cf30
navanjani <navanjani@outlook.com>
2022-09-23 12:39:01 +0200

    Add job application status to application seeds

--- seeders/20220903222012-add-initial-jobCandidateStatuses.js
+++ seeders/20220903222012-add-initial-jobCandidateStatuses.js
@@ -1,10 +1,26 @@
 "use strict";
 const { APPLICATION_STATUS_APPLIED } = require("../config/constants");
+const { job: Job } = require("../models");
 const JobCandidate = require("../models").jobCandidate;
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const job1 = await Job.findOne({
+      where: { title: "Senior Fullstack Developer" },
+    });
+    const job2 = await Job.findOne({
+      where: { title: "Full Stack JavaScript Developer" },
+    });
     const jobCandidate1 = await JobCandidate.findOne({
-      where: { email: "navanjani30@gmail.com" },
+      where: {
+        email: "navanjani30@gmail.com",
+        jobId: job1.id,
+      },
+    });
+    const jobCandidate2 = await JobCandidate.findOne({
+      where: {
+        email: "navanjani30@gmail.com",
+        jobId: job2.id,
+      },
     });
     await queryInterface.bulkInsert(
       "jobCandidateStatuses",
@@ -12,6 +28,12 @@ module.exports = {
         {
           status: APPLICATION_STATUS_APPLIED,
           jobCandidateId: jobCandidate1.id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          status: APPLICATION_STATUS_APPLIED,
+          jobCandidateId: jobCandidate2.id,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
